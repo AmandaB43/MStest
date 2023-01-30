6e9c410a15ae5821845545c64cc6265426b40b45
Amanda <amandarb43@gmail.com>
2023-01-29 19:08:39 -0800

    navBar fully functional, added in some text to each component/page, deleted invalid/unused code

--- mstest/src/App.js
+++ mstest/src/App.js
@@ -1,9 +1,44 @@
+import { BrowserRouter as Router, 
+  Link, Routes, Route } from 'react-router-dom'
+  import './styles.css';
 
-import React from 'react';
-import navBar from './components/navBar';
-function App(){
-  return (
-    navBar()
+import Recipes from './components/Recipes'
+import Home from './components/Home'
+import About from './components/About'
+import Reviews from './components/Reviews'
+
+function App () {
+    const recipes=['Peppered Chicken', 'Gourmet Burgers', 'Venice Pizza', 'Sushi Bake' ]
+
+    return(
+      <Router>
+          <div className="navBar">
+              <ul className='list'>
+                  <li>
+                   <Link to="/" className='home'>Home</Link>
+                  </li>
+                  <li>
+                   <Link to="/Recipes"className='rec'>Recipes</Link>
+                  </li>
+                  <li>
+                   <Link to="/About"className='about'>About Us</Link>
+                  </li>
+                  <li>
+                   <Link to="/Reviews"className='rev'>Reviews</Link>
+                  </li>
+
+              </ul>
+          </div>
+
+          <div className="display">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/reviews" element={<Reviews />} />
+              <Route path="/recipes" element={<Recipes recipes={recipes}/>} />
+            </Routes>
+          </div>
+      </Router>
   )
 }
 export default App;
--- mstest/src/components/Reviews.js
+++ mstest/src/components/Reviews.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
-export default function Recipes(){
+export default function Reviews(){
     return <h1> Reviews</h1>
 }
